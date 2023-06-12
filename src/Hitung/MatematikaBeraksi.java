8dca628301a56ed4ca9603bb823e2a2502f17738
godhomeavocados <rafikur333@gmail.com>
2023-06-12 21:40:26 +0800

    Membuat Class MatematikaBeraksi

--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -7,10 +7,10 @@ public class MatematikaBeraksi {
   public static void main (String[]args){
    //membuat objek
    Matematika Rafi = new Matematika(5,6);
-        System.out.println("Hasil penjumlahan   : "+ Rafi.setPenjumlahan());
-        System.out.println("Hasil pengurangan   : "+ Rafi.setPengurangan());
-        System.out.println("Hasil perkalian     : "+ Rafi.setPerkalian());
-        System.out.println("Hasil pembagian     : "+ Rafi.setPembagian());
+        System.out.println("Hasil Penjumlahan   : "+ Rafi.setPenjumlahan());
+        System.out.println("Hasil Pengurangan   : "+ Rafi.setPengurangan());
+        System.out.println("Hasil Perkalian     : "+ Rafi.setPerkalian());
+        System.out.println("Hasil Pembagian     : "+ Rafi.setPembagian());
         
               
   }  
