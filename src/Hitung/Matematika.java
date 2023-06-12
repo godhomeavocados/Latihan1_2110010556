8348e4865b262aed1dc527f37ba3514bfd049344
godhomeavocados <rafikur333@gmail.com>
2023-06-12 21:24:35 +0800

    Membuat constructor dan setPenjumlahan

--- src/Hitung/Matematika.java
+++ src/Hitung/Matematika.java
@@ -3,12 +3,38 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Latihan1;
+package Hitung;
 
 /**
  *
  * @author Administrator
  */
-public class Hitung {
+public class Matematika {
+    //membuat variable
+    private double bil1, bil2;
     
+    // membuat constructor
+    public Matematika (double bil1, double bil2){
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+        
+    }
+    //membuat setPenjumlahan
+    public double setPenjumlahan (){
+        return bil1 + bil2;
+        
+    }
+    public double setPengurangan (){
+        return bil1 - bil2;
+        
+    }
+    public double setPerkalian (){
+        return bil1 * bil2;
+        
+    }
+    public double setPembagian (){
+        return bil1 / bil2;
+        
+    }
+
 }
---
+++ test/MatematikaBeraksi.java
@@ -0,0 +1,15 @@
+
+
+
+public class MatematikaBeraksi {
+  public static void main (String[]args){
+   //membuat objek
+   Matematika Rafi = new Matematika(5,6);
+        System.out.println("Hasil penjumlahan   : "+ Rafi.setPenjumlahan());
+        System.out.println("Hasil pengurangan   : "+ Rafi.setPengurangan());
+        System.out.println("Hasil perkalian     : "+ Rafi.setPerkalian());
+        System.out.println("Hasil pembagian     : "+ Rafi.setPembagian());
+        
+        
+  }  
+}
